5a51d0fd849b136c75e552b26378730aaa06221c
samidahlawi <samialdahlawi@gmail.com>
2024-09-16 19:29:34 +0300

    complete the demo-page as mvp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import LiveKitComponent from "./LiveKitComponent";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <LiveKitComponent />
   </React.StrictMode>
 );
 
